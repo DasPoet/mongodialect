7d041d0d38760d0707dbd4a92e6a4952a931c4bc
Daspoet <cederk2306@gmail.com>
2021-04-09 16:39:34 +0200

    Tidy go.mod & Fix test setup

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.14
 
 require (
 	github.com/aws/aws-sdk-go v1.34.9 // indirect
-	github.com/fatih/structs v1.1.0
 	github.com/google/uuid v1.1.1
 	github.com/klauspost/compress v1.10.11 // indirect
 	github.com/mitchellh/mapstructure v1.3.3
--- test/test_setup.go
+++ test/test_setup.go
@@ -1,54 +1,56 @@
 package test
 
 import (
-    "context"
-    "github.com/google/uuid"
-    "github.com/informatik-q2/mongodialect"
-    "github.com/informatik-q2/mongodialect/interfaces"
+	"context"
+	"github.com/google/uuid"
+	"github.com/informatik-q2/mongodialect"
+	"github.com/informatik-q2/mongodialect/interfaces"
+	"reflect"
 )
 
 const (
-    hostname = "localhost"
-    database = "testBase"
-    port     = 27017
+	hostname = "localhost"
+	database = "testBase"
+	port     = 27017
 )
 
 type testDocument struct {
-    ID      uuid.UUID `bson:"ID, omitempty"`
-    Name    string    `bson:"name, omitempty"`
-    Surname string    `bson:"surname, omitempty"`
-    Age     uint      `bson:"age, omitempty"`
+	ID      uuid.UUID `bson:"ID, omitempty"`
+	Name    string    `bson:"name, omitempty"`
+	Surname string    `bson:"surname, omitempty"`
+	Age     uint      `bson:"age, omitempty"`
 }
 
 var obi = testDocument{
-    ID:      uuid.MustParse("822989de-fb98-40f8-a7e3-459277108b67"),
-    Name:    "General",
-    Surname: "Kenobi",
-    Age:     1,
+	ID:      uuid.MustParse("822989de-fb98-40f8-a7e3-459277108b67"),
+	Name:    "General",
+	Surname: "Kenobi",
+	Age:     1,
 }
 
 var ani = testDocument{
-    ID:      uuid.MustParse("f2c17a29-6ff6-4c9a-914f-ae635c84db89"),
-    Name:    "General",
-    Surname: "Skywalker",
-    Age:     0,
+	ID:      uuid.MustParse("f2c17a29-6ff6-4c9a-914f-ae635c84db89"),
+	Name:    "General",
+	Surname: "Skywalker",
+	Age:     0,
 }
 
 func makeRepository() interfaces.Repository {
-    uri := mongodialect.NewDatabaseURL(hostname, port)
-    driver := mongodialect.NewDriver(uri, database)
+	uri := mongodialect.NewDatabaseURL(hostname, port)
+	driver := mongodialect.NewDriver(uri, database)
 
-    if err := driver.OpenConnection(context.Background()); err != nil {
-        panic(err)
-    }
+	if err := driver.OpenConnection(context.Background()); err != nil {
+		panic(err)
+	}
 
-    repository, err := mongodialect.NewRepository(&testDocument{}, driver, "test", "ID")
+	base := reflect.TypeOf(new(testDocument))
+	repository, err := mongodialect.NewRepository(base, driver, "test", "ID")
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    return repository
+	return repository
 }
 
 var testRepository = makeRepository()
